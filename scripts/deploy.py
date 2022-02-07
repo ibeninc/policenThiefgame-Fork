fdce6c08bb9aa073c4a0624a4fbe2f70aa2635e3
ibeninc <royalrubee@gmail.com>
2022-02-07 23:37:57 +0100

    updated for testnet deploy

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,13 +1,18 @@
-from brownie import PoliceAndThief, config, network, config
+from brownie import Bank, config, network, config
 from scripts.helper import get_account
 from web3 import Web3
 
 
 def deploy_project():
     account = get_account()
+    myContract = Bank.deploy(
+        "0x3032E78978A96A7a0907883631aC227F542357e2",
+        "0x3032E78978A96A7a0907883631aC227F542357e2",
+        {"from": account},
+    )
     # if not in development use production pricefeed address for contract
-    if network.show_active() != "development":
-        myContract = PoliceAndThief.deploy({"from": account})
+    # if network.show_active() != "development":
+    #     myContract = PoliceAndThief.deploy('0x3032E78978A96A7a0907883631aC227F542357e2', '0x3032E78978A96A7a0907883631aC227F542357e2', {"from": account})
 
 
 def main():
